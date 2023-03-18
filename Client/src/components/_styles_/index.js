7058c03cf8d5c3737df674522a872a2b291248bd
awaisaltaf123 <awaisaltaf79@gmail.com>
2023-03-19 00:40:47 +0500

    add styled component for styling different compnent throughout the app

---
+++ Client/src/components/_styles_/index.js
@@ -0,0 +1,22 @@
+import Styled from "styled-components";
+
+export const Heading1 = Styled.h1`
+    display: flex;
+    justify-content: center;
+`;
+export const Heading2 = Styled.h2`
+    display: flex;
+    justify-content: end;
+    padding-right: 2%;
+`;
+
+export const TableWrapper = Styled.div`
+    height: 580px;
+    width: 96%;
+    padding: 2%;
+`;
+export const ErrorMsg = Styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+`;
